43c52d0170377df4f3ea3453d07e55b031feef9c
kingwin28 <jimfuwin1@163.com>
2015-05-30 14:47:43 +0800

    注册

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,23 @@ router.all('/registered', function(req, res, next) {
 
     var User = DB.get("User");
     var table = req.body;
-    return;
+    table.SEX = table.sex;
+
     User.insert(table,function(err,users){
-        console.log(users);
+        res.json(users);
+    })
+
+});
+//手机号码验证
+router.post('/mobile', function(req, res, next) {
+
+    var User = DB.get("User");
+    var table = req.body;
+    var mobile = table.mobile;
+    var sql = "select id from DML_USER where mobile = '"+mobile+"'";
+
+    User.countBySql(sql,function(err,count){
+        res.json(count);
     })
 
 });
